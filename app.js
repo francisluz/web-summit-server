22aa7ba542f6ec318b170c4502f44e41b0b018fe
Francis Fernandes da Luz <francisluz@Franciss-MacBook-Pro.local>
2017-05-21 19:08:29 -0300

    Initial commit as Server submodule

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express')
+var app = express()
+
+var myLogger = function (req, res, next) {
+  console.log('LOGGED')
+  next()
+}
+
+app.use(myLogger)
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.set('port', (process.env.PORT || 5000));
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
